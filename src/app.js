cacbed0a6199b91aedf625eb278090071b9cccc9
Baccin Michael <michael@MacBook-Air-di-Baccin.local>
2023-11-27 13:59:21 +0100

    modificato footer pagina help

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Michael Baccin',
     })
 })
 
